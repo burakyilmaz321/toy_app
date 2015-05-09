9617e4669eb27994a37c8ae7bb271af0b0be8993
Burak Yılmaz <burakyilmaz321@gmail.com>
2015-05-09 00:06:16 +0000

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "Burak Yılmaz", email: "burakyilmaz321@gmail.com")
